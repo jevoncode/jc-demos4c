5ff54cb1978c52f983ce382d33d8a7f2d291266a
jevoncode <jevoncode@gmail.com>
2014-12-01 01:43:51 +0800

    strlen which use pointer arithmetic

--- strlen/src/main.c
+++ strlen/src/main.c
@@ -11,7 +11,13 @@ int main() {
 	printf("'text' lenght is %d\n", jcstrlen("text"));
 	printf("lenght of name array is %d\n", jcstrlen(name));
 	printf("lenght of name array which p points to is %d\n", jcstrlen(p));
-	printf("out of bounds:%c",*(p+10));
+	printf("out of bounds:%c\n", *(p + 10));
+
+	printf("'text' lenght is %d\n", strlenUsePointArithmetic("text"));
+	printf("lenght of name array is %d\n", strlenUsePointArithmetic(name));
+	printf("lenght of name array which p points to is %d\n",
+			strlenUsePointArithmetic(p));
+
 	return 0;
 }
 
@@ -20,4 +26,11 @@ int jcstrlen(char *s) {
 	for (n = 0; *s != '\0'; s++)
 		n++;
 	return n;
+}
+
+int strlenUsePointArithmetic(char *s) {
+	char *p = s;
+	while (*p != '\0')
+		p++;
+	return p - s;
 }
